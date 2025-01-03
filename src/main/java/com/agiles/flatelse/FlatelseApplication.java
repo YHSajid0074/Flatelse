393823bda305dfa9a4c2d1d7e8d5bb6369d82993
Sajid <yeamimhossainsajid@gmail.com>
2025-01-03 15:54:36 +0600

    keepAliveScheduler added

--- src/main/java/com/agiles/flatelse/FlatelseApplication.java
+++ src/main/java/com/agiles/flatelse/FlatelseApplication.java
@@ -2,8 +2,10 @@ package com.agiles.flatelse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FlatelseApplication {
 
 	public static void main(String[] args) {
