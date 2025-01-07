b9733f3d1a35701174f4703cd7e71cf9b0b08494
Sajid <yeamimhossainsajid@gmail.com>
2025-01-07 15:24:05 +0600

    excel updated

--- src/main/java/com/agiles/flatelse/excel/ExcelReportService.java
+++ src/main/java/com/agiles/flatelse/excel/ExcelReportService.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -30,19 +32,26 @@ public class ExcelReportService {
         row.createCell(2).setCellValue("Email");
         row.createCell(3).setCellValue("Password");
 
+        row.setHeightInPoints(25);
+        CellStyle wrapStyle = workbook.createCellStyle();
+        wrapStyle.setWrapText(true); // Enable text wrapping
+
         int dataRow = 1;
         for (User user : users) {
             HSSFRow row1 = sheet.createRow(dataRow);
             row1.createCell(0).setCellValue(user.getId());
             row1.createCell(1).setCellValue(user.getUsername());
-            row1.createCell(3).setCellValue(user.getPassword());
             row1.createCell(2).setCellValue(user.getEmail());
+            Cell passwordCell = row1.createCell(3);
+            passwordCell.setCellValue(user.getPassword());
+            passwordCell.setCellStyle(wrapStyle);
+            row1.setHeightInPoints(30);
             dataRow++;
         }
 
         sheet.setColumnWidth(0, 4000); // ID column width
         sheet.setColumnWidth(1, 8000); // Name column width
-        sheet.setColumnWidth(2, 12000); // Email column width
+        sheet.setColumnWidth(2, 8000); // Email column width
         sheet.setColumnWidth(3, 8000);
 
         ServletOutputStream ops= response.getOutputStream();
