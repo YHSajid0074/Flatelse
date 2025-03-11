31a91e5e5958386c1f1b44950941086066d198f5
Sajid <yeamimhossainsajid@gmail.com>
2025-03-11 23:52:37 +0600

    Generic Added

--- src/main/java/com/agiles/flatelse/config/chat/chatting/ChatMessageService.java
+++ src/main/java/com/agiles/flatelse/config/chat/chatting/ChatMessageService.java
@@ -27,7 +27,7 @@ public class ChatMessageService {
     }
 
     public List<ChatMessage> findChatMessages(String senderId, String recipientId) {
-        var chatId = chatRoomService.getChatRoomId(senderId, recipientId, false);
+        var chatId = chatRoomService.getChatRoomId(senderId, recipientId, true);
         return chatId.map(chatMessageRepo::findByChatId).orElse(new ArrayList<>());
     }
 
